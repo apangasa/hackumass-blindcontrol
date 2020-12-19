d0e42b5b5bb6bf3139516152685027a35479f5d4
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-19 14:48:17 -0500

    Schedule instructions according to user defined schedule
    Missing request URLs

---
+++ cloud/scheduler.py
@@ -0,0 +1,17 @@
+import time
+import requests
+
+
+# Takes in a schedule dictionary and sends instructions at specified times
+# Exits when mode is changed to manual
+def scheduler(schedule):
+    manual = False
+    while not manual:
+        for scheduled_time in schedule.keys():
+            if int(time.strftime('%H')) == int(scheduled_time.split(':')[0]):
+                new_state = schedule[scheduled_time]
+                requests.post('FLIP STATE', json={'new_state': new_state})
+
+        manual = True if requests.get('GET MODE').json()[
+            'mode'] == 'manual' else False
+        schedule = requests.get('GET SCHEDULE').json()
