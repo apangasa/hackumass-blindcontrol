649d44c0cc1de514479deba22501348803430971
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-20 05:16:56 -0500

    Add comments to a few routes on Flask cloud endpoint

--- appEngineEndpoint/main.py
+++ appEngineEndpoint/main.py
@@ -9,23 +9,26 @@ app = Flask(__name__)
 CORS(app)
 
 # requests will be routed to this endpoint when it is time for an instruction to be executed
-LOCAL_ENDPOINT_URL = ''
+LOCAL_ENDPOINT_URL = 'http://393dca1f8152.ngrok.io'
+
+MODE = 'automatic'
+STATE = 'open'
+
 
 # A way to quickly check if the endpoint has deployed properly
-
-
 @app.route('/', methods=['GET'])
 def helloWorld():
     return 'Blind Control Endpoint is up and running!', 200
 
 
+# Returns the current mode (manual or automatic)
 @app.route('/get-mode', methods=['GET'])
 def getMode():
-    with open('./mode.data', 'r') as mode_file:
-        mode = mode_file.read()
-        return jsonify(mode=mode), 200
+    global MODE
+    return jsonify(mode=MODE), 200
 
 
+# Returns the current dictionary schedule with keys being time and values being 'open' or 'closed'
 @app.route('/get-schedule', methods=['GET'])
 def getSchedule():
     with open('./schedule.json', 'r') as schedule_file:
@@ -33,41 +36,43 @@ def getSchedule():
         return jsonify(schedule), 200
 
 
+# Sets the state (open or closed) based on a post request from the local endpoint
 @app.route('/receive-state', methods=['POST'])
 def receiveState():
+    global STATE
     state = request.json.get('state', None)
     if not state:
         return 'No state provided', 400
-    with open('./state.data', 'w') as state_file:
-        state_file.write(state)
+    STATE = state
     return jsonify(success=True), 200
 
 
+# Sends state to frontend app so the user knows the current state of their blinds at all times
 @app.route('/get-state', methods=['GET'])
 def getState():
-    with open('./state.data', 'r') as state_file:
-        state = state_file.read()
-        return jsonify(state=state), 200
+    global STATE
+    return jsonify(state=STATE), 200
+
 
 # Flask app will get a request from the frontend client application
 # Occurs when user presses button to change mode from manual to auto / vice versa
 # This endpoint will simply route this change directly to the local endpoint
-
-
 @app.route('/change-mode', methods=['POST'])
 def changeMode():
+    global MODE
     mode = request.json.get('mode', None)
     if not mode:
         return 'No mode specified', 400
+    MODE = mode
 
-    if mode == 'manual':
-        schedule = {}
-        with open('./schedule.json', 'r') as schedule_file:
-            schedule = json.load(schedule_file)
-        # run cloud function
-
-    with open('./mode.data', 'w') as mode_file:
-        mode_file.write(mode)
+    # The following code invokes the scheduler Cloud Function.
+    # It is commented out because full functionality along the pipeline for this has not been completely implemented.
+    # if mode == 'manual':
+    #     schedule = {}
+    #     with open('./schedule.json', 'r') as schedule_file:
+    #         schedule = json.load(schedule_file)
+    #           requests.post(
+    #               'https://us-east1-blind-control-299118.cloudfunctions.net/scheduler', json=schedule)
 
     requests.post(LOCAL_ENDPOINT_URL + '/write-mode', json={'mode': mode})
     return jsonify(success=True), 200
@@ -78,20 +83,21 @@ def changeMode():
 # Otherwise, it will throw an error and continue in auto
 @app.route('/flip', methods=['POST'])
 def openClose():
-    mode = None
-    with open('./mode.data', 'r') as mode_file:
-        mode = mode_file.read()
+    global MODE
+    global STATE
 
-    if mode == 'automatic':
+    if MODE == 'automatic':
         return 'Switch to manual!', 400
 
     new_state = request.json.get('new_state', None)
     if not new_state:
         return 'No new state specified', 400
-    # requests.post(LOCAL_ENDPOINT_URL + '/flip', json={'new_state': new_state})
+    requests.post(LOCAL_ENDPOINT_URL + '/flip', json={'new_state': new_state})
+    STATE = new_state
     return jsonify(success=True), 200
 
 
+# Dump the schedule dict {time: state} into a JSON file
 @app.route('/set-schedule', methods=['POST'])
 def setSchedule():
     schedule = request.json
